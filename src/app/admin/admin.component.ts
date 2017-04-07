119fd5f0d17305bfe49d519f45f474d23858c268
Sean Peterson <SeanPeterson11@gmail.com>
2017-04-07 10:40:48 -0700

    add ability to add a member to firebase

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [MemberService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+  }
+
+  submitForm(firstName: string, lastName: string, role: string, bio: string, ) {
+    var newMember: Member = new Member(firstName, lastName, role, bio);
+    this.memberService.addMember(newMember);
+    this.router.navigateByUrl('/');
   }
 
 }
