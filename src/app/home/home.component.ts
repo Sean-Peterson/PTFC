77d40bb2fe61e5c8c4b87d4de070445857d1b8a7
Sean Peterson <SeanPeterson11@gmail.com>
2017-04-07 10:44:21 -0700

    add ability to display all members on homepage component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [MemberService]
 })
 export class HomeComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) {}
 
   ngOnInit() {
+    this.members = this.memberService.getMembers()
   }
 
 }
